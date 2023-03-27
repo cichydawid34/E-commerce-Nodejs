3bb8cbee73fd364c1d7ab2f480d0934fa565c84c
Dawid Cichy <38354498+cichydawid34@users.noreply.github.com>
2023-03-27 16:30:18 +0200

    Add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors"
 import path from "path";
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //Routes
 
